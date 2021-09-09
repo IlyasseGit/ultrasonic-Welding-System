a0115897f7f48a61bcca2faa9f58ebd7c563bcad
ilyasse <76574221+IlyasseGit@users.noreply.github.com>
2021-09-09 20:40:57 +0200

    Update NN_p2.py

--- Neural network/NN_p2.py
+++ Neural network/NN_p2.py
@@ -21,28 +21,11 @@ data=pd.read_csv('clean_data_test.csv')
 data=np.matrix(data,float)
 X=data[:,1:13];
 Y=data[:,13:17];
-#
-#a=outliers_iqr(X)
-#
-#a[0,:] = list(dict.fromkeys(a[0,:]))
-#print(a) 
-#y=np.where(outlier[:,:]==True)
-#X = np.delete(X, y[0], axis=0)
-#Y = np.delete(Y, (y[0]), axis=0)
-#
-#X_train=X[:-12,:]
-#X_test=X[-12:,:]
-#
-#Y_train=Y[:-12,:]
-#Y_test=Y[-12:,:]
+
+
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size=0.2)
 
-
-# #############################################################################
-
-
 nn_reg=MLPRegressor(hidden_layer_sizes=(128,) ,activation='relu',solver='sgd',learning_rate='adaptive',max_iter=1000,learning_rate_init=0.003,alpha=0.35)
-
 
 nn_reg.fit(X_train, Y_train)
 Y_pred=nn_reg.predict(X_test)
@@ -66,15 +49,6 @@ x=[i for i in range(0,12)]
 plt.plot(x,Y_test[0:12,1],'b',Y_pred[0:12,1],'r')
 plt.title('Pression')
 plt.grid()
-#x=np.matrix(x)
-#a=np.where(Y_pred==241.16)
-
-#mean_absolute_error(Y_test[:,2], Y_pred[:,2])
-##np.sum(np.multiply((Y_test-Y_pred),(Y_test-Y_pred)))
-##accuracy_score(Y_test[:,0], Y_pred[:,0])
-#r2_score(Y_test[:,1], Y_pred[:,1])
-#mean_squared_error(Y_test[:,1], Y_pred[:,1])
-
 
 plt.figure(3)
 
@@ -84,20 +58,9 @@ plt.title('Amplitude')
 plt.grid()
 
 
-
 plt.figure(4)
 
 x=[i for i in range(0,12)]
 plt.plot(x,Y_test[0:12,3],'b',Y_pred[0:12,3],'r')
 plt.grid()
 plt.title('Width (mm)')
-
-#x=np.matrix(x)
-#a=np.where(Y_pred==241.16)
-
-#mean_absolute_error(Y_test[:,2], Y_pred[:,2])
-##np.sum(np.multiply((Y_test-Y_pred),(Y_test-Y_pred)))
-##accuracy_score(Y_test[:,0], Y_pred[:,0])
-#r2_score(Y_test[:,1], Y_pred[:,1])
-#mean_squared_error(Y_test[:,1], Y_pred[:,1])
-
